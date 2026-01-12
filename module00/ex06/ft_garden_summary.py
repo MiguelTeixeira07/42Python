9bf0756c96693ac41f46ae4adc13eb219c9d8248
MiguelTeixeira07 <miguelcasacateixeira@gmail.com>
2026-01-12 00:39:37 +0000

    Update ft_garden_summary.py

--- module00/ex06/ft_garden_summary.py
+++ module00/ex06/ft_garden_summary.py
@@ -1,5 +1,5 @@
 def ft_garden_summary():
     name = input("Enter Garden name: ")
-    plantNum = input("Enter number of plants: ")
+    plant_num = input("Enter number of plants: ")
 
-    print("Garden:", name, "\nPlants:", plantNum, "\nStatus: Growing well!")
+    print("Garden:", name, "\nPlants:", plant_num, "\nStatus: Growing well!")
