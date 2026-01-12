953f1580ab6919dc41f456aa2710729357289214
MiguelTeixeira07 <miguelcasacateixeira@gmail.com>
2026-01-12 00:01:31 +0000

    m00

---
+++ module00/ex02/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total():
+    h1 = int(input("Day 1 harvest: "))
+    h2 = int(input("Day 2 harvest: "))
+    h3 = int(input("Day 3 harvest: "))
+
+    print("Total harvest:", h1+h2+h3)
---
+++ module00/ex06/ft_garden_summary.py
@@ -0,0 +1,5 @@
+def ft_garden_summary():
+    name = input("Enter Garden name: ")
+    plantNum = input("Enter number of plants: ")
+
+    print("Garden:", name, "\nPlants:", plantNum, "\nStatus: Growing well!")
